50ded1e01b9dc7c51fc40489426b5e5d4a49835b
Neko-stack <gabriel.evil27@gmail.com>
2025-08-05 22:08:16 -0300

    reac

---
+++ React/introReactNeko/src/components/Motivos.jsx
@@ -0,0 +1,9 @@
+import './Motivos.css'
+
+function Motivos(){
+    return (
+        <nav className='motivos'>coisas </nav>
+    )
+}
+
+export default Motivos
---
+++ React/introReactNeko/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import './Navbar.css'
+
+function Navbar(){
+    return (
+        <nav>Home Contato Perfil </nav>
+    )
+}
+
+export default Navbar
